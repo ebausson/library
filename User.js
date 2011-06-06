95063207bd600221c0d669c563289445c6e45aa7
BAUSSON Etienne <ebausson@docxa.com>
2011-06-06 15:10:02 +0200

    sessions added, but not connected yet to authentication

--- User.js
+++ User.js
@@ -3,7 +3,7 @@ var User = exports;
 User.authenticate = function(request) {
 	var username = request.user;
 	var password = request.pass;
-	console.log(request);
+	console.log("*** Login attempt : " + request);
 	// todo : login
 	return username == password;
 }
--- app.js
+++ app.js
@@ -7,16 +7,28 @@ var User = require('./User.js');
 var DEBUG = true;
 
 var server = connect.createServer(
-	connect.logger(":status   :date - :url"),
+	connect.logger("*** :status   :date - :url"),
 	connect.favicon(__dirname + '/static/favicon.ico'),
+    connect.cookieParser(),
+    connect.session({secret:'some secret String'}),
 	connect.basicAuth(function(user, pass){
-		return User.authenticate({ user: user, pass: pass });
+		success = User.authenticate({ user: user, pass: pass });
+		return success;
 	})
 );
 
 server.use("/static", connect.static(__dirname + '/static'));
 server.use("/request", function(req, res) {
+	console.log("***");
+	console.log(req.session);
+	console.log("***");
+	console.log("***");
+	console.log(connect);
+	console.log("***");
+	console.log("***");
 	//TODO
+	
+	res.end("TODO");
 });
 server.use("/", function (req, res) {
 	getIndex(req, res);
@@ -30,7 +42,7 @@ getIndex = function(req, res) {
 	
 	fs.readFile(filename, function(err, file) {
 		if(err) {
-			console.log(err);
+			console.log("***  " + err);
 			res.writeHead(500, {"Content-Type": "text/plain"});
 			res.end(err + "\n");
 			return;
