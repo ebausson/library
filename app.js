9800fb7a09920e97acf24b54174be549e4e21f5b
BAUSSON Etienne <bausson.etienne@gmail.com>
2011-10-14 17:55:03 +0200

    some cleaning, ready to implement authentication.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // node API
-var fs = require("fs");							// filesystem API
-var url = require("url");						// URL parsing API
+var fs  = require("fs");                            // filesystem API
+var url = require("url");                        // URL parsing API
 
 // node modules
-var connect = require("connect");				// connect middleware
-var couchdb = require('couchdb');				// noSQL database
+var connect = require("connect");                // connect middleware
+var couchdb = require('couchdb');                // noSQL database
 
 // application modules
 var User = require('./js/User.js');
@@ -15,16 +15,16 @@ var User = require('./js/User.js');
 var DEBUG = true;
 
 var server = connect.createServer(
-	connect.logger("*** :status   :date - :url"),
-	connect.favicon(__dirname + '/static/favicon.ico'),
+    connect.logger("*** :status   :date - :url"),
+    connect.favicon(__dirname + '/static/favicon.ico'),
     connect.cookieParser(),
     connect.session({secret:'some secret String'})
 );
 
 
-var dbNamespace	= 'library_';
-var dbProvider	= couchdb.createClient(5984, '127.0.0.1');
-var db			= dbProvider.db(dbNamespace + 'db');
+var dbNamespace = 'library_';
+var dbProvider  = couchdb.createClient(5984, '127.0.0.1');
+var db          = dbProvider.db(dbNamespace + 'db');
 
 
 
@@ -39,40 +39,35 @@ server.use("/static", connect.static(__dirname + '/static'));
 
 // authentication request handling
 server.use("/auth", function(req, res) {
-	var parameters = getParameters(req.url);
-	var success = false;
-	if (parameters.user && parameters.password) {
-		// loggin attempt
-		success = User.authenticate(req.session, { user: parameters.user, pass: parameters.password });
-		//TODO
-	}
-	if (! success) {
-		console.log("********************");
-		console.log("**  loggin error  **");
-		console.log("********************");
-		console.log(parameters);
-		res.writeHead(401, {"Content-Type": "text/plain"});
-		res.end("Bad Credentials\n");
-		return;
-	}
-	
-	// now operations in case of success
-	console.log(req.session);
-	req.session.userlogin = parameters.user;
-	return success;
+    var parameters = getParameters(req.url);
+    var response = User.authenticate(db, parameters.user, parameters.password);
+    if (! response.success) {
+        res.writeHead(401, {"Content-Type": "text/plain"});
+    }
+    res.end(JSON.stringify(response));
+    return;
 });
+
+server.use("/account/create", function(req, res) {
+    var parameters = getParameters(req.url);
+    var response = User.createAccount(db, parameters.user, parameters.password);
+    
+    res.end(JSON.stringify(response));
+    return;
+});
+
 
 // other requests
 server.use("/request", function(req, res) {
-	User.isAuth(req, res) | function(req, res) {
-	//TODO
-	res.end("TODO")}();
+    User.isAuth(req, res) | function(req, res) {
+    //TODO
+    res.end("TODO")}();
 });
 
 
 // "root" handling
 server.use("/", function (req, res) {
-	getIndex(req, res);
+    getIndex(req, res);
 });
 
 server.listen(3000);
@@ -87,32 +82,32 @@ server.listen(3000);
 // *************
 
 function getIndex(req, res) {
-	var filename = __dirname + "/index.html";
-	
-	fs.readFile(filename, function(err, file) {
-		if(err) {
-			console.log(err);
-			res.writeHead(500, {"Content-Type": "text/plain"});
-			res.end(err + "\n");
-			return;
-		} else {
-			res.writeHead(200, {"Content-Type" : "text/html"});
-			res.end(file);
-		}
-	});
+    var filename = __dirname + "/index.html";
+    
+    fs.readFile(filename, function(err, file) {
+        if(err) {
+            console.log(err);
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            res.end(err + "\n");
+            return;
+        } else {
+            res.writeHead(200, {"Content-Type" : "text/html"});
+            res.end(file);
+        }
+    });
 };
 
 function getParameters(url) {
-	var response = {};
-	var query = url.split('?')[1];
-	if (query) {
-		queryEntries = query.split('&');
-		for (var i = 0; i < queryEntries.length; i++) {
-			var splittedQueryEntry = queryEntries[i].split('=');
-			if (splittedQueryEntry[1]) {
-				response[splittedQueryEntry[0]] = splittedQueryEntry[1];
-			}
-		}
-	}
-	return response;
+    var response = {};
+    var query = url.split('?')[1];
+    if (query) {
+        queryEntries = query.split('&');
+        for (var i = 0; i < queryEntries.length; i++) {
+            var splittedQueryEntry = queryEntries[i].split('=');
+            if (splittedQueryEntry[1]) {
+                response[splittedQueryEntry[0]] = splittedQueryEntry[1];
+            }
+        }
+    }
+    return response;
 }
--- js/User.js
+++ js/User.js
@@ -1,22 +1,44 @@
 var User = exports;
 
-User.authenticate = function(request, parameters) {
-	var username = parameters.user;
-	var password = parameters.pass;
-	console.log("*** Login attempt : ");
-	console.log(parameters);
-	//TODO : login
-	if (username == password) {
-		return true;
-	}
-	return false;
+User.authenticate = function(db, username, password) {
+    var response = {success : false};
+    if ( ! username || username == "") {
+        response.msg = 'Username not set';
+        return response;
+    }
+    if ( ! password || password == "") {
+        response.msg = 'Password not set';
+        return response;
+    }
+    console.log("*** Login attempt by : " + username);
+    
+    //TODO
+    
+    return response;
+}
+
+User.createAccount = function(db, username, password) {
+    var response = {success : false};
+    if ( ! username || username == "") {
+        response.msg = 'Username not set';
+        return response;
+    }
+    if ( ! password || password == "") {
+        response.msg = 'Password not set';
+        return response;
+    }
+    console.log("*** Account creation attempt with username : " + username);
+    
+    //TODO
+    
+    return response;
 }
 
 User.getLibrary = function() {
-	return {};
+    return {};
 }
 
 User.isAuth = function(req, res){
-	console.log(req.session);
-	return false;
+    console.log(req.session);
+    return false;
 }
